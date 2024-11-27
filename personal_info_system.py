9c787e135a384a22ee03c9ffce6a3c93b855dfc2
unknown <liamkyleyasona@gmail.com>
2024-11-28 04:07:49 +0800

    inputs

--- personal_info_system.py
+++ personal_info_system.py
@@ -1,3 +1,8 @@
 # Ask user for their personal information
 
-full_name = input("Please input your full name: ")
+with open("./Personal Information System.txt") as file_handle:
+    file_handle.write(full_name = input("Please input your name: "\n))
+    file_handle.write(address = input("Please input your name: "\n))
+    file_handle.write(age = input("Please input your name: "\n))
+    file_handle.write(email = input("Please input your name: "\n))
+    file_handle.write(address = input("Please input your name: "\n))
