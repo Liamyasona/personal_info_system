8ff7eda485c2719da191c32796777c4c90fed722
unknown <liamkyleyasona@gmail.com>
2024-11-28 04:19:20 +0800

    User inputs

--- personal_info_system.py
+++ personal_info_system.py
@@ -1,8 +1,15 @@
 # Ask user for their personal information
 
+while True:
+    full_name = input("Please input your name: ")
+    address = input("Please input your address: ")
+    age = input("Please input your age: ")
+    email = input("Please input your email: ")
+    contact_number = input("Please input your Contact Number: ")
+
 with open("./Personal Information System.txt") as file_handle:
-    file_handle.write(full_name = input("Please input your name: "\n))
-    file_handle.write(address = input("Please input your name: "\n))
-    file_handle.write(age = input("Please input your name: "\n))
-    file_handle.write(email = input("Please input your name: "\n))
-    file_handle.write(address = input("Please input your name: "\n))
+    file_handle.write()
+    file_handle.write()
+    file_handle.write()
+    file_handle.write()
+    file_handle.write()
