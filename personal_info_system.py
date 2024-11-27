ab22129ad115c18d9c55a243c85b1ce2942bee58
unknown <liamkyleyasona@gmail.com>
2024-11-28 06:58:10 +0800

    Open file command

--- personal_info_system.py
+++ personal_info_system.py
@@ -1,39 +1,43 @@
 # Ask user for their personal information
-
-# Loop to keep asking user for input
-while True:
-    print("Welcome! Please input your personal information.")
-
-    # Ask user for full name and print invalid when user inputs numbers
+with open("./Personal Information System.txt") as info_system:
+     
+    # Loop to keep asking user for input
     while True:
-        full_name = input("Please input your full name: ")
-        if full_name.isalpha() or " " in full_name:  
-                break  # Valid name
-        else:
-            print("Invalid Name. Input your correct name.")
+        print("Welcome! Please input your personal information.")
 
-    address = input("Please input your address: ")
+        # Ask user for full name and print invalid when user inputs numbers
+        while True:
+            full_name = input("Please input your full name: ")
+            if full_name.isalpha() or " " in full_name:  
+                    break  # Valid name
+            else:
+                print("Invalid Name. Input your correct name.")
 
-    # Print Invalid when age inputted is less than and equal to 0
-    while True:
-        age = int(input("Please input your age: "))
-        if age <= 0:
-            print("Invalid. Input the correct age")
-        else:
-             break
-        
-    email = input("Please input your email: ")
-    while True:
-        contact_number = int(input("Please input your Contact Number: "))
-        if len(contact_number) == 11:
-            break  
-        else:
-            print
+        address = input("Please input your address: ")
 
+        # Ask user for age and print invalid when age inputted is less than and equal to 0
+        while True:
+            age = int(input("Please input your age: "))
+            if age <= 0:
+                print("Invalid. Input the correct age")
+            else:
+                break
+            
+        # Ask user for email
+        email = input("Please input your email: ")
 
-with open("./Personal Information System.txt") as file_handle:
-    file_handle.write()
-    file_handle.write()
-    file_handle.write()
-    file_handle.write()
-    file_handle.write()
+        # Ask user for contact number and print invalid when number inputted is not in 11 digits
+        while True:
+            contact_number = int(input("Please input your Contact Number: "))
+            if len(contact_number) == 11:
+                break  
+            else:
+                print("Invalid Contact number. Please input a valid number")
+
+    # Every user input will be appended to the file
+
+        info_system.write(f"Full name: {full_name}\n")
+        info_system.write()
+        info_system.write()
+        info_system.write()
+        info_system.write()
