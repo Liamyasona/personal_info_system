e7792444122842451f1c13b0e624cae4e003c58f
unknown <liamkyleyasona@gmail.com>
2024-11-28 04:32:37 +0800

    Error handling for name and age

--- personal_info_system.py
+++ personal_info_system.py
@@ -1,11 +1,27 @@
 # Ask user for their personal information
 
+# Loop to keep asking user for input
 while True:
-    full_name = input("Please input your name: ")
+    print("Welcome! Please input your personal information.")
+
+    # Ask user for full name and print invalid when user inputs numbers
+    while True:
+        full_name = input("Please input your full name: ")
+        if full_name.isalpha() or " " in full_name:  
+                break  # Valid name
+        else:
+            print("Invalid Name. Input your correct name.")
+
     address = input("Please input your address: ")
-    age = input("Please input your age: ")
+
+    # Print Invalid when age inputted is less than and equal to 0
+    while True:
+        age = int(input("Please input your age: "))
+        if age <= 0:
+            print("Invalid. Input the correct age")
+
     email = input("Please input your email: ")
-    contact_number = input("Please input your Contact Number: ")
+    contact_number = int(input("Please input your Contact Number: "))
 
 with open("./Personal Information System.txt") as file_handle:
     file_handle.write()
