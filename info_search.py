b6406a885d635ee3e1d6f66451d132f615391d83
unknown <liamkyleyasona@gmail.com>
2024-11-28 12:18:02 +0800

    try and except

--- info_search.py
+++ info_search.py
@@ -1,6 +1,9 @@
 full_name_search = input("Please enter the full name to access the information:")
 
-with open("./Personal Information.txt", "r") as info_system:
-    lines = info_system.readlines()
-    for line in lines: 
-        
+try:
+    with open("./Personal Information.txt", "r") as info_system:
+        lines = info_system.readlines()
+        for line in lines: 
+            
+except FileNotFoundError:
+    print("The name is not found in the list.")
